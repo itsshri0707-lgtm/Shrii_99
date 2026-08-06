59f817aa7ae8c1578031fce1738a6c7c5c4bef23
itsshri0707-lgtm <itsshri0707@gmail.com>
2026-08-06 19:44:39 +0530

    Implement array reversal function
    Added a function to reverse an array and updated main to demonstrate its usage.

--- shrii.cpp
+++ shrii.cpp
@@ -1,1 +1,26 @@
+void reverse(int arr[],int size){
 
+    int start = 0;
+    int end = size-1;
+
+    while(start < end){
+        swap(arr[start],arr[end]);
+        start++;
+        end--;
+    }
+}
+
+int main(){
+    
+    int size = 7;
+    int arr[size] ={32,33,98,77,11,89,17};
+    
+    reverse(arr,size);
+
+    for(int i = 0;i<size;i++){
+        cout<<arr[i]<<" ";
+    }
+
+    return 0;
+
+}

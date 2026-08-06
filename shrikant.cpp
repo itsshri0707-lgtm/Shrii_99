af0763bdce4c99dc80a076af1b5993c74d47d138
itsshri0707-lgtm <itsshri0707@gmail.com>
2026-08-06 20:12:38 +0530

    Add files via upload

---
+++ shrikant.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+using namespace std;
+void reverse(int arr[],int size){
+
+    int start = 0;
+    int end = size-1;
+
+    while(start < end){
+        swap(arr[start],arr[end]);
+        start++;
+        end--;
+    }
+}
+
+int main(){
+    
+    int size = 7;
+    int arr[size] ={32,33,98,77,11,89,17};
+    
+    reverse(arr,size);
+
+    for(int i = 0;i<size;i++){
+        cout<<arr[i]<<" ";
+    }
+
+    return 0;
+
+}
